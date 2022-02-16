0aaad17beb96df44559def92ea56ab2d73b03799
John-Pena <jmp1495@gmail.com>
2022-02-15 22:19:24 -0600

    created fight function inside js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+// code below defines the function
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// code below calls on the function to deploy
+fight ();
+
+// Code below will capture input data from player and deploy it
+var playerName = window.prompt("What is your robot's name");
+
+//SeniorPena si ready for battle!
+console.log("SeniorPena" + " is ready for battle!");
+//"SeniorPena the robot is ready for battle!""
+console.log(playerName + " is ready for battle!");
+// "Your robot, SeniorPena, has won!"
+console.log("Your robot, " + playerName + ", has won!");
